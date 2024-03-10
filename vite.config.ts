13134bd891601e6ab83141c223a86d41cfc5db79
Ata Kuyumcu <ata@mm.st>
2024-03-10 17:34:31 +0100

    fix: move github action coverage config to workspace vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: "istanbul",
+      reporter: ["text", "json-summary", "json"],
     },
     globals: true,
     environment: "jsdom",
